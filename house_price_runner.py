511f8ef62a2b1b2d3e5a67e179254e3a7137ed2d
LalliJ <jemjit.lalli@bca.com>
2022-11-17 17:41:04 +0000

    added generic preprocessing, removes outlier detection and cleans data for NAs, need to add tests

--- house_price_runner.py
+++ house_price_runner.py
@@ -1,3 +1,12 @@
 from utils.load.data_importing import import_csv_data
+from utils.preprocessing.generic_preprocessing import clean_data, remove_all_outliers
+
 
 data = import_csv_data("data\house_pricing.csv")
+data = clean_data(data_frame=data)
+columns_for_outlier_detection = list(["price", "lotsize_sq_ft"])
+data = remove_all_outliers(
+    data_frame=data,
+    columns_for_outlier_detection=columns_for_outlier_detection,
+    method="elpitic_envelope",
+)
