22f4ad85b77184e6d7e8aa3f93696e57f400e7a3
LalliJ <jemjit.lalli@bca.com>
2022-11-19 23:44:17 +0000

    tests for feature engineering, needs bolstering

---
+++ tests/features/test_feature_engineering.py
@@ -0,0 +1,18 @@
+import os
+
+import pandas as pd
+
+from utils.features.feature_engineering import engineer_features
+from utils.load.data_importing import import_csv_data
+
+path_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+
+def test_engineer_features():
+    test_data = import_csv_data(f"{path_dir}\\data\\house_pricing.csv")
+    test_data_features = engineer_features(test_data)
+
+    assert isinstance(
+        test_data_features, pd.DataFrame
+    ), "returned dataframe from feature engineering did not return pd.DataFrame"
+    # TODO: write further tests
