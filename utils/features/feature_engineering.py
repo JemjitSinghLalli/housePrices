b7c7cd88846bd1be023bf7131dde22b3b7901771
LalliJ <jemjit.lalli@bca.com>
2022-11-19 23:15:58 +0000

    simple feature engineering completed, price broken down by features

--- utils/features/feature_engineering.py
+++ utils/features/feature_engineering.py
@@ -3,6 +3,16 @@ import pandas as pd
 
 
 def engineer_features(data_frame: pd.DataFrame) -> pd.DataFrame:
+    """
+    Dataframe holding house prices which engineers features.
+    Args:
+        data_frame: The dataframe holding columns to feature engineer.
+    Returns: the dataframe with engineered features
+    """
+    data_frame["price_per_sq_ft"] = data_frame["price"] / data_frame["lotsize_sq_ft"]
+    data_frame["price_per_bedroom"] = data_frame["price"] / data_frame["bedrooms"]
+    data_frame["price_per_bathroom"] = data_frame["price"] / data_frame["bathrooms"]
+    data_frame["price_per_stories"] = data_frame["price"] / data_frame["stories"]
 
     print("Feature engineering is complete.")
 
